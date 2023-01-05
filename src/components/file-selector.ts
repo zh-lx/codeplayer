057e8788a309d14f6075422c07986441c2701adc
zh-lx <18366276315@163.com>
2023-01-05 09:16:12 +0800

    feat: 文件添加删除

--- src/components/file-selector.ts
+++ src/components/file-selector.ts
@@ -2,7 +2,7 @@ import { ReplStore } from '../core/store';
 import './file-selector.css';
 import { MapFile } from '../constant';
 import { Editor } from 'codemirror';
-import { getMode } from '../utils';
+import { getMode, validateFile } from '../utils';
 interface FileSelectorOptions {
   store: ReplStore;
   el: HTMLElement;
@@ -15,6 +15,7 @@ export default class FileSelector {
   activeFileItem: HTMLElement;
   mainFileItem: HTMLElement;
   editorInstance: Editor;
+  fileInput: HTMLInputElement | null;
   constructor(options: FileSelectorOptions) {
     this.store = options.store;
     this.el = options.el;
@@ -32,7 +33,9 @@ export default class FileSelector {
     selector.classList.add('code-sandbox-file-selector');
     selector.innerHTML = `<div class="code-sandbox-file-selector-left"></div>
     <div class="code-sandbox-file-selector-right"></div>`;
-    const left = selector.querySelector('.code-sandbox-file-selector-left');
+    const left = selector.querySelector(
+      '.code-sandbox-file-selector-left'
+    ) as HTMLElement;
     const right = selector.querySelector('.code-sandbox-file-selector-right');
     for (let file in this.store.state.files) {
       const fileItem = this.createFileItem(file) as HTMLElement;
@@ -42,6 +45,7 @@ export default class FileSelector {
         left?.append(fileItem);
       }
     }
+    this.createAddFileButton(left);
     return selector;
   }
 
@@ -103,5 +107,51 @@ export default class FileSelector {
     fileItem.querySelector('svg')?.addEventListener('click', deleteFile);
 
     return fileItem;
+  }
+
+  // 添加文件
+  createAddFileButton(parentNode: HTMLElement) {
+    const button = document.createElement('div');
+    button.classList.add('code-sandbox-add-file-button');
+    button.id = '__add-file-button';
+    button.innerHTML = `
+    <svg width="1em" height="1em" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M10 44H38C39.1046 44 40 43.1046 40 42V14H30V4H10C8.89543 4 8 4.89543 8 6V42C8 43.1046 8.89543 44 10 44Z" fill="none" stroke="#606266" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M30 4L40 14" stroke="#606266" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M24 21V35" stroke="#606266" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M17 28H24L31 28" stroke="#606266" stroke-width="3" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>`;
+    button.addEventListener('click', () => {
+      if (!this.fileInput) {
+        this.fileInput = this.createFilenameInput(parentNode);
+        parentNode.insertBefore(this.fileInput, button);
+      }
+      this.fileInput.focus();
+    });
+    parentNode.append(button);
+  }
+
+  // 文件名添加输入框
+  createFilenameInput(parentNode: HTMLElement) {
+    const input = document.createElement('input');
+    input.classList.add('code-sandbox-filename-input');
+    input.placeholder = '请输入新文件名';
+    input.addEventListener('blur', () => {
+      const filename = input.value;
+      const validate = validateFile(filename);
+      if (validate) {
+        this.fileInput = null;
+        this.store.addFile(filename);
+        const newFileItem = this.createFileItem(filename);
+        parentNode.insertBefore(newFileItem, input);
+        input.remove();
+        // this.activeFileItem.classList.remove('code-sandbox-file-active-item');
+        // this.activeFileItem = newFileItem;
+        // newFileItem.classList.add('code-sandbox-file-active-item');
+        // this.editorInstance.setValue(this.store.state.activeFile.code);
+        // this.editorInstance.setOption('mode', getMode(filename));
+      }
+    });
+    return input;
   }
 }
--- src/utils/editor.ts
+++ src/utils/editor.ts
@@ -5,3 +5,15 @@ export const getMode = (filename: string) => {
     ? 'css'
     : 'javascript';
 };
+
+export const validateFile = (filename: string) => {
+  const result = ['.html', '.css', '.js', '.ts', '.vue'].some((item) =>
+    filename.endsWith(item)
+  );
+  if (!result) {
+    window.alert(
+      'Code Sandbox 当前仅支持以 *.html、*.css、*.js、*.ts、*.vue 的文件'
+    );
+  }
+  return result;
+};
