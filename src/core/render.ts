113e36a3eff0c9ba798ffe63195b0397b077979a
zh-lx <18366276315@163.com>
2023-01-09 09:00:37 +0800

    style: 修改样式

--- src/core/render.ts
+++ src/core/render.ts
@@ -1,7 +1,4 @@
-import { ReplStore, StoreOptions } from './store';
-import { compileModulesForPreview } from './module-compiler';
-import { createSandbox } from '../components/sandbox';
-import { createCodeMirror } from '../components/codemirror';
+import { StoreOptions } from './store';
 import CodeSandbox from '../components/code-sandbox';
 
 export const Sandbox = async ({
